4294f5defe1414bb2e446ca9efa9ca7a19a52b39
anwam <dev.anuwong@gmail.com>
2024-02-11 00:49:47 +0700

    feat: create CRUD endpoints

---
+++ internal/cars/cars.go
@@ -0,0 +1,7 @@
+package cars
+
+type Car struct {
+	Name     string `json:"name"`
+	Price    int64  `json:"price"`
+	Discount int64  `json:"discount"`
+}
--- main.go
+++ main.go
@@ -1,3 +1,48 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.GET("/cars", func(c echo.Context) error {
+		return c.JSON(200, map[string]interface{}{
+			"hello": "world",
+		})
+	})
+
+	e.POST("/cars", func(c echo.Context) error {
+		return c.JSON(200, map[string]interface{}{
+			"hello": "world",
+		})
+
+	})
+
+	e.PATCH("/cars/:id", func(c echo.Context) error {
+		id := c.Param("id")
+		return c.JSON(200, map[string]interface{}{
+			"hello": "world",
+			"id":    id,
+		})
+	})
+
+	e.DELETE("/cars/:id", func(c echo.Context) error {
+		id := c.Param("id")
+		return c.JSON(200, map[string]interface{}{
+			"hello":   "world",
+			"message": "cars " + id + " has been deleted.",
+		})
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
